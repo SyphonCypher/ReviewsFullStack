ccc4dbdc02ec2931dbeb116d99db37c9feef4cf0
SyphonCypher <tim_horta@outlook.com>
2018-10-19 14:02:44 -0400

    initial commit

---
+++ ReviewsSite/Models/Review.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewsSite.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Category { get; set; }
+        public string Descrip { get; set; }
+        public string Content { get; set; }
+    }
+}
